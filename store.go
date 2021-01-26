214c064abb8be2a262b7a46f3c9e9cd20fa01bda
Marcos Huck <marcos@huck.com.ar>
2021-01-26 09:37:49 -0300

    Add store.

---
+++ store.go
@@ -0,0 +1,10 @@
+package raft
+
+// Key represents the identification key for a value inside the state machine.
+type Key string
+
+// Value represents the data type saved inside the state machine.
+type Value float64
+
+// Store represents a map of Key and Value. It's used to represent the state machine.
+type Store map[Key]Value
